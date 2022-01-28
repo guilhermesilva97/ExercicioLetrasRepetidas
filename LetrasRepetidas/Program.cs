0c68eaa201dcca10251d19ea7eb9ab193923ba9e
Guilherme Silva <guilhermeadasilva@outlook.com.br>
2022-01-28 11:47:45 -0300

    Commit Letras

---
+++ LetrasRepetidas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace LetrasRepetidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] duplicados = { "abracadabra", "allottee", "assessee", "kelless", "keenness" };
+
+            var anterior = " ";
+
+            foreach (var palavra in duplicados)
+            {
+                var letras = from
+                    carac in palavra
+                           select carac;
+
+                foreach (var item in letras)
+                {
+                   if (item != Convert.ToChar(anterior))
+                    {
+                        Console.Write(item);
+                    }
+                    anterior = Convert.ToString(item);
+                }
+                anterior = " ";
+                Console.WriteLine("");
+            }
+        }
+
+    }
+}
